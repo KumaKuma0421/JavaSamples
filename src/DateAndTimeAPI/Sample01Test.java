79ec862997a04cb3db5f35b700224e64c8c7a7dd
kumakuma0421 <kumakuma0421@gmail.com>
2018-09-26 01:09:28 +0900

    テストをJUnit5でやってみます。
    ちょっとテストドライバーのファイル位置が良くないですが。

---
+++ src/DateAndTimeAPI/Sample01Test.java
@@ -0,0 +1,32 @@
+package DateAndTimeAPI;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Sample01Test extends Sample01 {
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void testSample1() {
+        this.sample1();
+    }
+
+}

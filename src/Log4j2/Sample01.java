f7f94cec6cba10c6068e416746946d75bde77440
KumaKuma0421 <kumakuma0421@gmail.com>
2018-10-08 00:49:34 +0900

    ログ関係を追加します。

---
+++ src/Log4j2/Sample01.java
@@ -0,0 +1,27 @@
+package Log4j2;
+
+import java.util.stream.IntStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Sample01 {
+    Logger myLogger = LogManager.getLogger(this.getClass());
+
+    public void Test01() {
+        this.myLogger.trace("{}", "arg1");
+        this.myLogger.debug("{}", "arg1");
+        this.myLogger.info("{}", "arg1");
+        this.myLogger.warn("{} {}", "arg1", "arg2");
+        this.myLogger.error("{}", "arg1");
+    }
+
+    public void Test02() {
+        var loop = IntStream.rangeClosed(1, 10000);
+        loop.forEach((i) -> {
+            this.myLogger.info("count {}", i);
+            this.Test01();
+        });
+    }
+
+}
